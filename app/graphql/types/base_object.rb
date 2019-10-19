0c734138c9b5e6a6bbb5f23728cd122978e28b19
Michael Kelly <michaelkelly322@gmail.com>
2019-10-18 22:22:09 -0400

    Adds syntax sugar for field defs

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,5 +1,12 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
     field_class Types::BaseField
+
+    # Syntax sugar so we don't have to type `null: true` all the time
+    #
+    # def field(*args, **kwargs, &block)
+    #   kwargs[:null] = true if kwargs[:null].nil? && kwargs[:mutation].nil?
+    #   super
+    # end
   end
 end
